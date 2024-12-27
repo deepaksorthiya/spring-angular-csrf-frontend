c90e2ac617a181206962d2fca7b4cdb89138bb67
Deepak Katariya <deepak.sorthiya@gmail.com>
2024-12-27 13:29:44 +0530

    spring angular csrf config

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'spring-angular-csrf-frontend' title`, () => {
+  it(`should have the 'angular-19-csrf-frontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('spring-angular-csrf-frontend');
+    expect(app.title).toEqual('angular-19-csrf-frontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, spring-angular-csrf-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-19-csrf-frontend');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'spring-angular-csrf-frontend';
+  title = 'angular-19-csrf-frontend';
 }
